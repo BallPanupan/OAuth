40ed0516f2c15790109ad88c20334cb17861df2c
Panupan <ball085573@hotmail.com>
2022-11-01 11:32:21 +0700

    delete

--- index.js
+++ index.js
@@ -2,12 +2,6 @@ require('dotenv').config()
 const express = require('express');
 const bodyParser = require('body-parser')
 const port = 3000
-const jwt = require('jsonwebtoken')
-const { CheckRefToken } = require('./module/CheckRefToken')
-const { CheckUser } = require('./module/CheckUser')
-const { DeleteLoginToken } = require('./module/DeleteLoginToken')
-const { InsertLoginToken } = require('./module/InsertLoginToken')
-const { Register } = require('./module/Register')
 
 // express app
 const app = express();
