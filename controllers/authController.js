9364539ef8baacb154e6200a399aa5b1af450935
Panupan <ball085573@hotmail.com>
2022-11-01 11:31:58 +0700

    move function to controller

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,10 @@
 const { generateAccessToken } = require("../module/generateAccessToken")
 const jwt = require('jsonwebtoken')
 const { Register } = require("../module/Register")
+const { CheckUser } = require("../module/CheckUser")
+const { InsertLoginToken } = require("../module/InsertLoginToken")
+const { CheckRefToken } = require("../module/CheckRefToken")
+const { DeleteLoginToken } = require("../module/DeleteLoginToken")
 exports.main = (req, res) => {
   res.json({
     'status': true,
@@ -19,5 +23,76 @@ exports.register = async (req, res) => {
     res.json({accessToken:prepareData.accessToken, refreshToken: prepareData.refreshToken})
   } else {
     res.json({status:'error'})
+  }
+}
+
+exports.posts = async (req, res) => {
+  let userResult = await CheckUser({username:req.user.username})
+  if(userResult.status){
+    res.json(userResult.data)
+  }else{
+    res.sendStatus(401)
+  }
+}
+
+exports.login = async (req, res) => {
+  const username = req.body.username
+  let userResult = await CheckUser({username:username})
+  if(userResult.status){
+    const userData = {
+      "id": userResult.data.id,
+      "username": userResult.data.username,
+      "type_id": userResult.data.type_id,
+    }
+
+    const refreshToken = jwt.sign(userData, process.env.REFRESH_TOKEN_SECRET)
+
+    InsertLoginToken({ id:userResult.data.id, refreshToken:refreshToken })
+
+    res.json({
+      accessToken: generateAccessToken(userData),
+      refreshToken: refreshToken
+    })
+  }else{
+    res.sendStatus(401)
+  }
+}
+
+exports.newToken = async (req, res) => {
+  const refreshToken = req.body.token
+
+  if(refreshToken == null ) return res.sendStatus(401)
+
+  // check token on Database
+  let userToken = await CheckRefToken({refreshToken:refreshToken})
+
+  if(userToken.status){
+    jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
+      if(err) return res.sendStatus(403)
+      const userData = {
+        "id": user.id,
+        "username": user.username,
+        "type_id": user.type_id,
+      }
+      const accessToken = generateAccessToken(userData)
+      res.json({accessToken: accessToken})
+    })
+  }else {
+    res.sendStatus(401)
+  }
+}
+
+exports.logout = async (req, res) => {
+  // refreshTokens = refreshTokens.filter(token => token !== req.body.token)
+  let userData = await CheckRefToken({refreshToken:req.body.token})
+  if(userData.status){
+    let result = await DeleteLoginToken({tokenId:userData.result.id})
+    if(result.status){
+      res.sendStatus(204)
+    }else{
+      res.sendStatus(401)
+    }
+  }else{
+    res.sendStatus(401)
   }
 }
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const authController = require('../controllers/authController');
+const { AuthenticateToken } = require('../module/AuthenticateToken');
 
 const router = express.Router();
 
-// router.get('/', authController.main);
+router.get('/', authController.main);
 router.post('/register', authController.register);
-// router.get('/posts', authController.posts);
-// router.get('/login', authController.login);
-// router.get('/newToken', authController.newToken);
-// router.get('/logout', authController.logout);
+router.get('/posts', AuthenticateToken, authController.posts);
+router.post('/login', authController.login);
+router.post('/newToken', authController.newToken);
+router.delete('/logout', authController.logout);
 
 
 module.exports = router;
