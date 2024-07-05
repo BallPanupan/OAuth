8d8af83cc73829b110132bac1d992b27d8202cff
Panupan <Ball085573@hotmail.com>
2024-07-05 17:53:42 +0700

    style(vender)

--- controllers/auth/index.js
+++ controllers/auth/index.js
@@ -1,14 +1,13 @@
-const { _register } = require("./registerController");
-const { _wellcome } = require("./wellcomeController");
-const { _getProfile } = require("./getProfileController");
-const { _login } = require("./login");
-const { _refreshToken } = require("./refreshToken");
-
+const { _register     } = require("./registerController"  );
+const { _wellcome     } = require("./wellcomeController"  );
+const { _getProfile   } = require("./getProfileController");
+const { _login        } = require("./login"               );
+const { _refreshToken } = require("./refreshToken"        );
 
 module.exports = {
-	wellcome: _wellcome,
-	register: _register,
-	profile: _getProfile,
-	login: _login,
+	wellcome    : _wellcome    ,
+	register    : _register    ,
+	profile     : _getProfile  ,
+	login       : _login       ,
 	refreshToken: _refreshToken,
 }
